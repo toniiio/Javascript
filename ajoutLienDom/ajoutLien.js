43a9e79c7d0211cdbf78e3a944d9492bc30f04de
toniiio <58595515+toniiio@users.noreply.github.com>
2020-12-10 22:06:18 +0100

    Creation de lien et formulaire Dom

---
+++ ajoutLienDom/ajoutLien.js
@@ -0,0 +1,115 @@
+﻿var listeLiens = [
+    {
+        titre: "So Foot",
+        url: "http://sofoot.com",
+        auteur: "yann.usaille"
+    },
+    {
+        titre: "Guide d'autodéfense numérique",
+        url: "http://guide.boum.org",
+        auteur: "paulochon"
+    },
+    {
+        titre: "L'encyclopédie en ligne Wikipedia",
+        url: "http://Wikipedia.org",
+        auteur: "annie.zette"
+    }
+];
+
+
+function creerElementLien(lien) {
+    var titreElt = document.createElement("a");
+    titreElt.href = lien.url;
+    titreElt.style.color = "#428bca";
+    titreElt.style.textDecoration = "none";
+    titreElt.style.marginRight = "5px";
+    titreElt.appendChild(document.createTextNode(lien.titre));
+
+    var urlElt = document.createElement("span");
+    urlElt.appendChild(document.createTextNode(lien.url));
+
+    
+    var ligneTitreElt = document.createElement("h4");
+    ligneTitreElt.style.margin = "0px";
+    ligneTitreElt.appendChild(titreElt);
+    ligneTitreElt.appendChild(urlElt);
+
+    
+    var ligneDetailsElt = document.createElement("span");
+    ligneDetailsElt.appendChild(document.createTextNode("Ajouté par " + lien.auteur));
+
+    var divLienElt = document.createElement("div");
+    divLienElt.classList.add("lien");
+    divLienElt.appendChild(ligneTitreElt);
+    divLienElt.appendChild(ligneDetailsElt);
+
+    return divLienElt;
+}
+
+var contenuElt = document.getElementById("contenu");
+
+listeLiens.forEach(function (lien) {
+    var lienElt = creerElementLien(lien);
+    contenuElt.appendChild(lienElt);
+});
+
+function creerElementInput(placeholder, taille) {
+    var inputElt = document.createElement("input");
+    inputElt.type = "text";
+    inputElt.setAttribute("placeholder", placeholder);
+    inputElt.setAttribute("size", taille);
+    inputElt.setAttribute("required", "true");
+    return inputElt;
+}
+
+var ajouterLienElt = document.getElementById("ajoutLien");
+
+ajouterLienElt.addEventListener("click", function () {
+    var auteurElt = creerElementInput("Entrez votre nom", 20);
+    var titreElt = creerElementInput("Entrez le titre du lien", 40);
+    var urlElt = creerElementInput("Entrez l'URL du lien", 40);
+
+    var ajoutElt = document.createElement("input");
+    ajoutElt.type = "submit";
+    ajoutElt.value = "Ajouter";
+
+    var formAjoutElt = document.createElement("form");
+    formAjoutElt.appendChild(auteurElt);
+    formAjoutElt.appendChild(titreElt);
+    formAjoutElt.appendChild(urlElt);
+    formAjoutElt.appendChild(ajoutElt);
+
+    var p = document.querySelector("p");
+    p.replaceChild(formAjoutElt, ajouterLienElt);
+
+    formAjoutElt.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+        
+        var url = urlElt.value;
+        if ((url.indexOf("http://") !== 0) && (url.indexOf("https://") !== 0)) {
+            
+            url = "http://" + url;
+        }
+      
+        var lien = {
+            titre: titreElt.value,
+            url: url,
+            auteur: auteurElt.value
+        };
+
+        var lienElt = creerElementLien(lien);       
+        contenuElt.insertBefore(lienElt, contenuElt.firstChild);
+
+        p.replaceChild(ajouterLienElt, formAjoutElt);
+
+  
+        var infoElt = document.createElement("div");
+        infoElt.classList.add("info");
+        infoElt.textContent = "Le lien \"" + lien.titre + "\" a bien été ajouté.";
+        p.insertBefore(infoElt, ajouterLienElt);
+       
+        setTimeout(function () {
+            p.removeChild(infoElt);
+        }, 2000);
+    });
+});
